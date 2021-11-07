374e6207028a56592a5d8f5b664c393edcd6b9a7
tobiasz.porebski <tobiasz.porebski@gmail.com>
2021-11-07 10:25:04 +0100

    Add Triangle shape functionality and use it to generate a fractal

--- 04_cha/Shape.py
+++ 04_cha/Shape.py
@@ -1,31 +1,106 @@
 import random
-from typing import Tuple
+import math
+from typing import Tuple, List
+
 
 class _Shape:
 
+    def __init__(self, plane_size: Tuple[int, int, int, int] ):#"minX, minY, maxX, maxY of a plane as a touple"):
 
-
-    def __init__(self, plane_size: "minX, minY, maxX, maxY of a plane as a touple"):
-
-        if not(plane_size is Tuple) or len(plane_size) != 4:
-            raise Exception("Wrong format of a plane size.")
+        # if (plane_size is not tuple) or len(plane_size) != 4:
+        #     raise Exception("Wrong format of a plane size.")
             
         self.plane_size = plane_size
         self.last_vertice = None
         self.choosen_vertice = None
-        self.vertices = {}
+        self.vertices = []
+
+    def get_last_vertice(self) -> Tuple[float, float]:
+        return self.last_vertice
+
+    def get_next_vertice(self) -> Tuple[float, float]:
+        pass
+
+    def get_random_point_inside(self) -> Tuple[float,float]:
+        pass
+
+    def get_all_polygon_vertices(self) -> List[float]:
+        result =[]
+        if len(self.vertices) != 0:
+            for v in self.vertices:
+                result.append(v[0])
+                result.append(v[1])
+
+        return result
+
+    # def add_vertice(self, pos_x: float, posy_y: float) -> None:
+    #     """Add vertice if is within the plane. Otherwise raise an exception"""
+    #
+    #     if pos_x < self.plane_size[0] or \
+    #         posy_y < self.plane_size[1] or \
+    #         pos_x > self.plane_size[2] or \
+    #         posy_y > self.plane_size[3]:
+    #         raise Exception("Given vertice is outside of the plane.")
+    #
+    #     self.vertices.append(pos_x,posy_y)
 
 
-    def add_vertice(self, pos_x: float, posy_y: float) -> None:
-        """Add vertice if is within the plane. Otherwise raise an exception"""
-
-        if pos_x < self.plane_size[0] or posy_y < self.plane_size[1] or pos_x > self.plane_size[2] or posy_y > self.plane_size[3]:
-            raise Exception("Given vertice is outside of the plane.")
-
-        self.vertices.add(pos_x,posy_y)
 
 
-    def get_vertice(self) -> Tuple[float,float]:
+class Triangle(_Shape):
+
+    def __init__(self, plane_size: Tuple[int, int, int, int]):
+        super().__init__(plane_size)
+        triangle_height = self.plane_size[3] - self.plane_size[1]
+        triangle_width = self.plane_size[2] - self.plane_size[0]
+        if triangle_height < (triangle_width/2) * math.sqrt(3):
+            triangle_width = 2 * (triangle_height / math.sqrt(3))
+        else:
+            triangle_height = (triangle_width / 2) * math.sqrt(3)
+        #1st vertice - top
+        v1_y = self.plane_size[1]       # Y grows downwards
+        v1_x = self.plane_size[2] / 2  # must be in the middle
+        #2nd vertice - right corner
+        v2_y = triangle_height
+        v2_x = triangle_width/2 + v1_x
+        #3rd vertice - left corner
+        v3_y = v2_y
+        v3_x = v2_x - triangle_width
+
+        # self.triangle_height = triangle_height
+        # self.triangle_width = triangle_width
+
+        self.vertices.append((v1_x, v1_y))
+        self.vertices.append((v2_x, v2_y))
+        self.vertices.append((v3_x, v3_y))
+
+    def get_random_point_inside(self) -> Tuple[float,float]:
+        # TO DO: return a true random point inside the triangle
+        point_y = random.randint( int(self.vertices[0][1]),
+                                  int(self.vertices[1][1])
+                                  )
+        point_x = self.vertices[0][0]
+        return (point_x,point_y)
+
+    def get_next_vertice(self) -> Tuple[float,float]:
+        """Return a random vertice: (x,y) . Raise Excepcion if there is no vertice added."""
+
+        if len(self.vertices) == 0:
+            raise Exception("List of vertices is empty.")
+        else:
+            self.last_vertice = self.choosen_vertice
+            # self.vertice_pool = self.vertices - self.last_vertice
+            self.choosen_vertice = random.choice(self.vertices)
+
+        return self.choosen_vertice
+
+
+class Square(_Shape):
+
+    def __init__(self, plane_size: Tuple[int, int, int, int]):
+        super().__init__(plane_size)
+
+    def get_next_vertice(self) -> Tuple[float,float]:
         """Return a random vertice: (x,y) . Raise Excepcion if there is no vertice added."""
 
         if len(self.vertices) == 0:
@@ -36,5 +111,6 @@ class _Shape:
             self.choosen_vertice = random.choice(self.vertices)
 
         return self.choosen_vertice
+
 
 
--- 04_cha/main.py
+++ 04_cha/main.py
@@ -1,5 +1,15 @@
+import tkinter
 from tkinter import *
 from random import randint,choice
+from Shape import Triangle
+
+CANVAS_HEIGHT = 1000
+CANVAS_WIDTH = 1000
+shape = Triangle(plane_size=(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT))
+step_counter = 0
+is_on = False
+is_running = True
+point = shape.get_random_point_inside()
 
 def start():
     global is_on
@@ -9,22 +19,27 @@ def stop():
     global is_on
     is_on = False
 
+def reset(canvas: tkinter.Canvas, counter_label: tkinter.Label):
+    global step_counter
+    step_counter = 0
+    stop()
+    canvas.delete('all')
+    counter_label.configure(text=str(step_counter))
+    canvas.create_polygon(shape.get_all_polygon_vertices(), outline="white")
+    make_next_point(shape.get_next_vertice())
+
 def make_next_point(vertex):
     global point
+    vertex = shape.get_next_vertice()
     new_x = (vertex[0] + point[0]) // 2
     new_y = (vertex[1] + point[1]) // 2
     point = [new_x, new_y]
 
-vertex1 = (2,868)
-vertex2 = (1002,868)
-vertex3 = (502,2)
-vertex_list = [vertex1, vertex2, vertex3]
-point = [-1,-1]
+def on_closing():
+    global is_running
+    is_running = False
 
-canvas_height = 870
-canvas_width = 1004
-step_counter = 0
-is_on = False
+
 
 window = Tk()
 frame1 = Frame(window)
@@ -32,7 +47,7 @@ frame1.grid(row = 0, column = 0)
 frame2 = Frame(window)
 frame2.grid(row = 0, column = 1)
 
-canvas = Canvas(frame1, height = canvas_height, width = canvas_width, bg="black")
+canvas = Canvas(frame1, height = CANVAS_HEIGHT, width = CANVAS_WIDTH, bg="black")
 canvas.pack()
 label1 = Label(frame2, text="Step:", font=("Arial",12) )
 label1.pack()
@@ -42,23 +57,30 @@ btn_start = Button(frame2, text="START", bg= "green", fg="white", command=start)
 btn_start.pack(pady=5)
 btn_stop = Button(frame2, text="STOP", bg= "red", fg="white", command=stop)
 btn_stop.pack(pady=5)
+btn_reset = Button(frame2, text="RESET", bg= "brown", fg="white", command=lambda: reset(canvas, label2))
+btn_reset.pack(pady=5)
 
 #Create RECTANGLE
-canvas.create_polygon([vertex1[0], vertex1[1], vertex2[0], vertex2[1], vertex3[0], vertex3[1] ],
-                        outline="white")
+# canvas.create_polygon([vertex1[0], vertex1[1], vertex2[0], vertex2[1], vertex3[0], vertex3[1] ],
+#                         outline="white")
+# canvas.create_polygon( shape.get_all_polygon_vertices(), outline="white")
 
-#Choose first point
-point[0] = randint(vertex1[0], vertex2[0])
-point[1] = randint(vertex3[1], vertex2[1])
+#Make first iteration
+# make_next_point(shape.get_next_vertice())
+
+reset(canvas, label2)
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
 
 #mainloop
-while True:
+while is_running:
     window.update()
     if is_on:
-        make_next_point(choice(vertex_list) )
+        make_next_point(shape.get_next_vertice() )
         canvas.create_rectangle(point[0], point[1], point[0], point[1], outline="white",fill="white")
         step_counter += 1
-        label2.configure(text=str(step_counter) )
+        label2.configure(text=str(step_counter))
 
 
 
